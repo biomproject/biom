ab86524c6f6e9a947c07ded2525c3cea55006fa6
mezoistvan <mezoistvan1@gmail.com>
2019-04-16 06:16:36 +0200

    Add nicer shine

--- Assets/Scripts/_graphics/PlayerCell.cs
+++ Assets/Scripts/_graphics/PlayerCell.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class PlayerCell : MonoBehaviour {
     public HexCoordinates coordinates;
-    private Animator anim;
+    private Animator wallAnim;
     private Transform shine;
     private Transform redBloodCells;
+    private Transform wall;
 
     void Awake() {
-        anim = GetComponent<Animator>();
         foreach (Transform child in transform) {
             if (child.name == "Shine") {
                 shine = child;
@@ -18,11 +18,15 @@ public class PlayerCell : MonoBehaviour {
             if (child.name == "RedBloodCells") {
                 redBloodCells = child;
             }
+            if (child.name == "Wall") {
+                wall = child;
+                wallAnim = child.GetComponent<Animator>();
+            }
         }
     }
 
     public void PlayTargetingAnim() {
-        anim.Play("bubble_target");
+        wallAnim.Play("bubble_target");
         SpriteRenderer sr = shine.GetComponent<SpriteRenderer>();
         sr.transform.Translate(1000, 1000, 1000);
         SpriteRenderer sr2 = redBloodCells.GetComponent<SpriteRenderer>();
@@ -30,9 +34,9 @@ public class PlayerCell : MonoBehaviour {
     }
     public void PlayCellWallAnim(PlayerCellWallCase wallCase) {
         Tuple<PlayerCellWallCase, int> archeTypeAndDegree = PlayerCellWall.GetArcheTypeAndDegree(wallCase);
-        anim.Play(archeTypeAndDegree.Item1.ToString());
-        Quaternion target = Quaternion.Euler(90, (float)archeTypeAndDegree.Item2, 0);
-        transform.eulerAngles =  new Vector3(
+        wallAnim.Play(archeTypeAndDegree.Item1.ToString());
+        Quaternion target = Quaternion.AngleAxis((float)archeTypeAndDegree.Item2, Vector3.up);
+        wall.eulerAngles =  new Vector3(
             transform.eulerAngles.x,
             archeTypeAndDegree.Item2,
             transform.eulerAngles.z
@@ -41,9 +45,9 @@ public class PlayerCell : MonoBehaviour {
 
     public void PlayCellWallAnim(string wallCase) {
         Tuple<PlayerCellWallCase, int> archeTypeAndDegree = PlayerCellWall.GetArcheTypeAndDegree(wallCase);
-        anim.Play(archeTypeAndDegree.Item1.ToString());
+        wallAnim.Play(archeTypeAndDegree.Item1.ToString());
         Quaternion target = Quaternion.AngleAxis((float)archeTypeAndDegree.Item2, Vector3.up);
-        transform.eulerAngles =  new Vector3(
+        wall.eulerAngles =  new Vector3(
             transform.eulerAngles.x,
             archeTypeAndDegree.Item2,
             transform.eulerAngles.z
