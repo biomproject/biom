8b9cfd8b47e5d6eed7ea1233e61d3f1b8846418f
mezoistvan <mezoistvan1@gmail.com>
2019-05-26 14:53:33 +0200

    Add loader icon

--- Assets/Scripts/MenuObject.cs
+++ Assets/Scripts/MenuObject.cs
@@ -8,17 +8,36 @@ public class MenuObject : MonoBehaviour {
     bool isTitleUp = true;
     bool isTutorialUp = false;
     Animator anim;
+    ScriptUsageTimeline scriptUsageTimeline;
+    bool waitingForBeat = false;
+    int previousBeat = 0;
+
+    GameObject loader;
 
     void Awake() {
         anim = GetComponent<Animator>();
+        scriptUsageTimeline = GameObject.Find("Music Player").GetComponent<ScriptUsageTimeline>();
+        loader = GameObject.Find("Loader");
+        loader.transform.Translate(1000, 1000, 1000);
     }
 
     void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            if (isTitleUp) {
+        if (waitingForBeat) {
+		    if (previousBeat != scriptUsageTimeline.timelineInfo.currentMusicBar) {
+                loader.transform.Translate(1000, 1000, 1000);
+                waitingForBeat = false;
                 isTitleUp = false;
                 isTutorialUp = true;
                 anim.Play("title_tut_anim");
+            }
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (isTitleUp) {
+                loader.transform.Translate(-1000, -1000, -1000);
+                previousBeat = scriptUsageTimeline.timelineInfo.currentMusicBar;
+                waitingForBeat = true;
                 return;
             }
 
